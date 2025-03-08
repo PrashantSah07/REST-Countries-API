fb878de071800f835c0c18e9834d0b02b69aab44
prashant <prashantkumar855114@gmail.com>
2025-03-08 13:57:07 +0530

    set darkmode in local storage

--- src/CountryCard.jsx
+++ src/CountryCard.jsx
@@ -35,7 +35,9 @@ const CountryCard = () => {
 
     return (
         <>
-            <Navbar />
+            <header>
+                <Navbar />
+            </header>
             <button className='back-button'><Link to="/" ><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M22.0003 12.9999L22.0004 11L8.41421 11V5.58582L2 12L8.41421 18.4142L8.41421 13L22.0003 12.9999Z"></path></svg>Back</Link></button>
             {Loading && <p style={{ display: 'flex', justifyContent: 'center' }}><img className='loadingspinner' src={LoadingSpinner} alt="Loading..." /></p>}
             {notFound && <h1 style={{ color: 'red', textAlign: 'center' }}>Country not found!</h1>}
